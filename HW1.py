dfb914c4a71bc1f47be790b45506fb9cdc97bb1f
Ken <kendrick.yuen41@myhunter.cuny.edu>
2020-12-28 22:20:12 -0500

    Finished HW1

--- HW1.py
+++ HW1.py
@@ -4,26 +4,36 @@ print("My name is Koala")  # replace <NAME> with your name
 
 # Create a variable called "num" and assign it to an int 55. Add 1 to it and print its new value.
 
-
+num = 55
+print(num + 1)
 
 squareRoot = '1 ** 0.5'  # If you were to print this variable, what would the output be? Comment your answer below.
+
+# 1 ** 0.5
+
 squareRoot = 1 ** 0.5  # What about the value of this?
+
+# 1
 
 # Create a variable assigned to any floating point value and print it.
 
-b = 4.5
-print (b)
+b = 5.6
+print(b)
 
 # Print the remainder of 20 divided by 3 using the designated math operator.
 
-print ("20 % 3")
+print(20 % 3)
 
 # Create two int variables, "a" and "b", and add them together in a print statement.
 
-
+a = 56
+b = 44
+print(a+b)
 
 not (not (not ((13 % 15 == 0) and (True or False))))  # What is the value of this? Comment below.
 
+# True
 
 # Print whether a variable is positive using a comparison operator.
 c = -10
+print(c > 0)
