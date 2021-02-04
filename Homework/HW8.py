3832548bedefb7df71c21a512e00b124d80035bd
Ken <kendrick.yuen41@myhunter.cuny.edu>
2021-02-04 14:30:04 -0500

    Testing solution for Problem3

--- Homework/HW8.py
+++ Homework/HW8.py
@@ -58,7 +58,7 @@ return False since 1/31/2021 is a Sunday.
 import datetime
 
 def is_weekday(date_obj):
-    if date.weekday() < 5:
+    if datetime.datetime.date_obj().weekday() < 5:
         return True
     else:
         return False
